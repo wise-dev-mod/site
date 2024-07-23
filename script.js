28775dcaf43d13dfc125300a2141831dfb7e3fbd
Sebastien Pfrimmer <mamiebio67@gmail.com>
2024-07-23 12:26:39 +0200

    Release

--- script.js
+++ script.js
@@ -8,27 +8,28 @@ window.addEventListener('scroll', () => {
     const scrollPosition = window.scrollY / (document.body.scrollHeight - window.innerHeight);
     const currentScrollTop = window.scrollY;
 
+    // Changer la couleur de fond en fonction de la position de défilement
+    if (scrollPosition === 0) {
+        document.body.style.backgroundColor = 'white';
+    } else if (scrollPosition === 1) {
+        document.body.style.backgroundColor = 'pink';
+    } else {
+        const maxHue = 20; // Ajuster cette valeur pour l'effet désiré
+        const hue = scrollPosition * maxHue;
+        document.body.style.backgroundColor = `hsl(${hue}, 100%, 88%)`;
+    }
+
+    // Autres effets de défilement
     if (scrollPosition > 0.1) {
         flowerContainer.style.opacity = Math.min(1, scrollPosition);
         scrollText.style.opacity = Math.min(1, (scrollPosition - 0.1) * 2);
 
-        // Rotate the flower based on scroll position
+        // Rotation de la fleur en fonction de la position de défilement
         const rotation = scrollPosition * 360;
         flower.style.transform = `rotate(${rotation}deg)`;
     } else {
         flowerContainer.style.opacity = 0;
         scrollText.style.opacity = 0;
-    }
-
-    // Change background color based on scroll direction
-    if (currentScrollTop > lastScrollTop) {
-        // Scrolling down
-        const maxHue = 330; // Adjust this value for desired effect
-        const hue = scrollPosition * maxHue;
-        document.body.style.backgroundColor = `hsl(${hue}, 100%, 88%)`;
-    } else {
-        // Scrolling up
-        document.body.style.backgroundColor = `white`;
     }
 
     lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
